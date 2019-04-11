aed00f1200d663b14fb1952290ad1bf2c3a8c47f
hjx_glb <hjx_glb@qq.com>
2019-04-11 21:27:01 +0800

    the first

--- src/main/java/com/cssl/Test/Test1.java
+++
@@ -1,5 +0,0 @@
-package com.cssl.Test;
-
-public class Test1 {
-
-}
---
+++ src/main/java/com/cssl/TestGit/Test1.java
@@ -0,0 +1,7 @@
+package com.cssl.TestGit;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("胡佳禧正在测试！！！");
+    }
+}
